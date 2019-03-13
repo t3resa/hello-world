144563898b769d2dd694838ae635fcef48bf150a
teresa.lachina <teresa.lachina@accenture.com>
2019-03-13 16:28:20 -0400

    commented somethings out

--- jenkins/jobs/dsl/skeleton_pipeline_jobs.groovy
+++ jenkins/jobs/dsl/skeleton_pipeline_jobs.groovy
@@ -24,9 +24,9 @@ def logRotatorArtifactsNumToKeep = 7
 // Jobs
 def buildAppJob = freeStyleJob(projectFolderName + "/Skeleton_Application_Build")
 def unitTestJob = freeStyleJob(projectFolderName + "/Skeleton_Application_Unit_Tests")
-def codeAnalysisJob = freeStyleJob(projectFolderName + "/Skeleton_Application_Code_Analysis")
-def deployJob = freeStyleJob(projectFolderName + "/Skeleton_Application_Deploy")
-def regressionTestJob = freeStyleJob(projectFolderName + "/Skeleton_Application_Regression_Tests")
+//def codeAnalysisJob = freeStyleJob(projectFolderName + "/Skeleton_Application_Code_Analysis")
+//def deployJob = freeStyleJob(projectFolderName + "/Skeleton_Application_Deploy")
+//def regressionTestJob = freeStyleJob(projectFolderName + "/Skeleton_Application_Regression_Tests")
 
 // Views
 def pipelineView = buildPipelineView(projectFolderName + "/Skeleton_Application")
@@ -47,40 +47,40 @@ pipelineView.with{
 
 buildAppJob.with{
   description("Skeleton application build job.")
-  logRotator {
-    daysToKeep(logRotatorDaysToKeep)
-    numToKeep(logRotatorBuildNumToKeep)
-    artifactDaysToKeep(logRotatorArtifactsNumDaysToKeep)
-    artifactNumToKeep(logRotatorArtifactsNumToKeep)
-  }
-  scm scmProvider.get(projectScmNamespace, skeletonAppgitRepo, "*/master", "adop-jenkins-master", null)
+  // logRotator {
+  //   daysToKeep(logRotatorDaysToKeep)
+  //   numToKeep(logRotatorBuildNumToKeep)
+  //   artifactDaysToKeep(logRotatorArtifactsNumDaysToKeep)
+  //   artifactNumToKeep(logRotatorArtifactsNumToKeep)
+  // }
+  // scm scmProvider.get(projectScmNamespace, skeletonAppgitRepo, "*/master", "adop-jenkins-master", null)
   environmentVariables {
       env('WORKSPACE_NAME',workspaceFolderName)
       env('PROJECT_NAME',projectFolderName)
   }
-  label("docker")
-  wrappers {
-    preBuildCleanup()
-    injectPasswords()
-    maskPasswords()
-    sshAgent("adop-jenkins-master")
-  }
-  triggers scmProvider.trigger(projectScmNamespace, skeletonAppgitRepo, "master")
-  steps {
-    shell('''## YOUR BUILD STEPS GO HERE'''.stripMargin())
-  }
-  publishers{
-    downstreamParameterized{
-      trigger(projectFolderName + "/Skeleton_Application_Unit_Tests"){
-        condition("UNSTABLE_OR_BETTER")
-        parameters{
-          predefinedProp("B",'${BUILD_NUMBER}')
-          predefinedProp("PARENT_BUILD", '${JOB_NAME}')
-        }
-      }
-    }
-  }
-}
+//   label("docker")
+//   wrappers {
+//     preBuildCleanup()
+//     injectPasswords()
+//     maskPasswords()
+//     sshAgent("adop-jenkins-master")
+//   }
+//   triggers scmProvider.trigger(projectScmNamespace, skeletonAppgitRepo, "master")
+//   steps {
+//     shell('''## YOUR BUILD STEPS GO HERE'''.stripMargin())
+//   }
+//   publishers{
+//     downstreamParameterized{
+//       trigger(projectFolderName + "/Skeleton_Application_Unit_Tests"){
+//         condition("UNSTABLE_OR_BETTER")
+//         parameters{
+//           predefinedProp("B",'${BUILD_NUMBER}')
+//           predefinedProp("PARENT_BUILD", '${JOB_NAME}')
+//         }
+//       }
+//     }
+//   }
+// }
 
 unitTestJob.with{
   description("This job runs unit tests on our skeleton application.")
@@ -88,147 +88,147 @@ unitTestJob.with{
     stringParam("B",'',"Parent build number")
     stringParam("PARENT_BUILD","Skeleton_Application_Build","Parent build name")
   }
-  logRotator {
-    daysToKeep(logRotatorDaysToKeep)
-    numToKeep(logRotatorBuildNumToKeep)
-    artifactDaysToKeep(logRotatorArtifactsNumDaysToKeep)
-    artifactNumToKeep(logRotatorArtifactsNumToKeep)
-  }
-  wrappers {
-    preBuildCleanup()
-    injectPasswords()
-    maskPasswords()
-    sshAgent("adop-jenkins-master")
-  }
+  // logRotator {
+  //   daysToKeep(logRotatorDaysToKeep)
+  //   numToKeep(logRotatorBuildNumToKeep)
+  //   artifactDaysToKeep(logRotatorArtifactsNumDaysToKeep)
+  //   artifactNumToKeep(logRotatorArtifactsNumToKeep)
+  // }
+  // wrappers {
+  //   preBuildCleanup()
+  //   injectPasswords()
+  //   maskPasswords()
+  //   sshAgent("adop-jenkins-master")
+  // }
   environmentVariables {
       env('WORKSPACE_NAME',workspaceFolderName)
       env('PROJECT_NAME',projectFolderName)
   }
-  label("docker")
-  steps {
-  }
-  steps {
-    shell('''## YOUR UNIT TESTING STEPS GO HERE'''.stripMargin())
-  }
-  publishers{
-    downstreamParameterized{
-      trigger(projectFolderName + "/Skeleton_Application_Code_Analysis"){
-        condition("UNSTABLE_OR_BETTER")
-        parameters{
-          predefinedProp("B",'${B}')
-          predefinedProp("PARENT_BUILD",'${PARENT_BUILD}')
-        }
-      }
-    }
-  }
-}
+//   label("docker")
+//   steps {
+//   }
+//   steps {
+//     shell('''## YOUR UNIT TESTING STEPS GO HERE'''.stripMargin())
+//   }
+//   publishers{
+//     downstreamParameterized{
+//       trigger(projectFolderName + "/Skeleton_Application_Code_Analysis"){
+//         condition("UNSTABLE_OR_BETTER")
+//         parameters{
+//           predefinedProp("B",'${B}')
+//           predefinedProp("PARENT_BUILD",'${PARENT_BUILD}')
+//         }
+//       }
+//     }
+//   }
+// }
 
-codeAnalysisJob.with{
-  description("This job runs code quality analysis for our skeleton application using SonarQube.")
-  parameters{
-    stringParam("B",'',"Parent build number")
-    stringParam("PARENT_BUILD","Skeleton_Application_Build","Parent build name")
-  }
-  logRotator {
-    daysToKeep(logRotatorDaysToKeep)
-    numToKeep(logRotatorBuildNumToKeep)
-    artifactDaysToKeep(logRotatorArtifactsNumDaysToKeep)
-    artifactNumToKeep(logRotatorArtifactsNumToKeep)
-  }
-  environmentVariables {
-      env('WORKSPACE_NAME',workspaceFolderName)
-      env('PROJECT_NAME',projectFolderName)
-  }
-  wrappers {
-    preBuildCleanup()
-    injectPasswords()
-    maskPasswords()
-    sshAgent("adop-jenkins-master")
-  }
-  label("docker")
-  steps {
-    shell('''## YOUR CODE ANALYSIS STEPS GO HERE'''.stripMargin())
-  }
-  publishers{
-    downstreamParameterized{
-      trigger(projectFolderName + "/Skeleton_Application_Deploy"){
-        condition("UNSTABLE_OR_BETTER")
-        parameters{
-          predefinedProp("B",'${B}')
-          predefinedProp("PARENT_BUILD", '${PARENT_BUILD}')
-        }
-      }
-    }
-  }
-}
+// codeAnalysisJob.with{
+//   description("This job runs code quality analysis for our skeleton application using SonarQube.")
+//   parameters{
+//     stringParam("B",'',"Parent build number")
+//     stringParam("PARENT_BUILD","Skeleton_Application_Build","Parent build name")
+//   }
+//   logRotator {
+//     daysToKeep(logRotatorDaysToKeep)
+//     numToKeep(logRotatorBuildNumToKeep)
+//     artifactDaysToKeep(logRotatorArtifactsNumDaysToKeep)
+//     artifactNumToKeep(logRotatorArtifactsNumToKeep)
+//   }
+//   environmentVariables {
+//       env('WORKSPACE_NAME',workspaceFolderName)
+//       env('PROJECT_NAME',projectFolderName)
+//   }
+//   wrappers {
+//     preBuildCleanup()
+//     injectPasswords()
+//     maskPasswords()
+//     sshAgent("adop-jenkins-master")
+//   }
+//   label("docker")
+//   steps {
+//     shell('''## YOUR CODE ANALYSIS STEPS GO HERE'''.stripMargin())
+//   }
+//   publishers{
+//     downstreamParameterized{
+//       trigger(projectFolderName + "/Skeleton_Application_Deploy"){
+//         condition("UNSTABLE_OR_BETTER")
+//         parameters{
+//           predefinedProp("B",'${B}')
+//           predefinedProp("PARENT_BUILD", '${PARENT_BUILD}')
+//         }
+//       }
+//     }
+//   }
+// }
 
-deployJob.with{
-  description("This job deploys the skeleton application to the CI environment")
-  parameters{
-    stringParam("B",'',"Parent build number")
-    stringParam("PARENT_BUILD","Skeleton_Application_Build","Parent build name")
-    stringParam("ENVIRONMENT_NAME","CI","Name of the environment.")
-  }
-  logRotator {
-    daysToKeep(logRotatorDaysToKeep)
-    numToKeep(logRotatorBuildNumToKeep)
-    artifactDaysToKeep(logRotatorArtifactsNumDaysToKeep)
-    artifactNumToKeep(logRotatorArtifactsNumToKeep)
-  }
-  wrappers {
-    preBuildCleanup()
-    injectPasswords()
-    maskPasswords()
-    sshAgent("adop-jenkins-master")
-  }
-  environmentVariables {
-      env('WORKSPACE_NAME',workspaceFolderName)
-      env('PROJECT_NAME',projectFolderName)
-  }
-  label("docker")
-  steps {
-    shell('''## YOUR DEPLOY STEPS GO HERE'''.stripMargin())
-  }
-  publishers{
-    downstreamParameterized{
-      trigger(projectFolderName + "/Skeleton_Application_Regression_Tests"){
-        condition("UNSTABLE_OR_BETTER")
-        parameters{
-          predefinedProp("B",'${B}')
-          predefinedProp("PARENT_BUILD", '${PARENT_BUILD}')
-          predefinedProp("ENVIRONMENT_NAME", '${ENVIRONMENT_NAME}')
-        }
-      }
-    }
-  }
-}
+// deployJob.with{
+//   description("This job deploys the skeleton application to the CI environment")
+//   parameters{
+//     stringParam("B",'',"Parent build number")
+//     stringParam("PARENT_BUILD","Skeleton_Application_Build","Parent build name")
+//     stringParam("ENVIRONMENT_NAME","CI","Name of the environment.")
+//   }
+//   logRotator {
+//     daysToKeep(logRotatorDaysToKeep)
+//     numToKeep(logRotatorBuildNumToKeep)
+//     artifactDaysToKeep(logRotatorArtifactsNumDaysToKeep)
+//     artifactNumToKeep(logRotatorArtifactsNumToKeep)
+//   }
+//   wrappers {
+//     preBuildCleanup()
+//     injectPasswords()
+//     maskPasswords()
+//     sshAgent("adop-jenkins-master")
+//   }
+//   environmentVariables {
+//       env('WORKSPACE_NAME',workspaceFolderName)
+//       env('PROJECT_NAME',projectFolderName)
+//   }
+//   label("docker")
+//   steps {
+//     shell('''## YOUR DEPLOY STEPS GO HERE'''.stripMargin())
+//   }
+//   publishers{
+//     downstreamParameterized{
+//       trigger(projectFolderName + "/Skeleton_Application_Regression_Tests"){
+//         condition("UNSTABLE_OR_BETTER")
+//         parameters{
+//           predefinedProp("B",'${B}')
+//           predefinedProp("PARENT_BUILD", '${PARENT_BUILD}')
+//           predefinedProp("ENVIRONMENT_NAME", '${ENVIRONMENT_NAME}')
+//         }
+//       }
+//     }
+//   }
+// }
 
-regressionTestJob.with{
-  description("This job runs regression tests on the deployed skeleton application")
-  parameters{
-    stringParam("B",'',"Parent build number")
-    stringParam("PARENT_BUILD","Skeleton_Application_Build","Parent build name")
-    stringParam("ENVIRONMENT_NAME","CI","Name of the environment.")
-  }
-  logRotator {
-    daysToKeep(logRotatorDaysToKeep)
-    numToKeep(logRotatorBuildNumToKeep)
-    artifactDaysToKeep(logRotatorArtifactsNumDaysToKeep)
-    artifactNumToKeep(logRotatorArtifactsNumToKeep)
-  }
-  scm scmProvider.get(projectFolderName, regressionTestGitRepo, "*/master", "adop-jenkins-master", null)
-  wrappers {
-    preBuildCleanup()
-    injectPasswords()
-    maskPasswords()
-    sshAgent("adop-jenkins-master")
-  }
-  environmentVariables {
-      env('WORKSPACE_NAME',workspaceFolderName)
-      env('PROJECT_NAME',projectFolderName)
-  }
-  label("docker")
-  steps {
-    shell('''## YOUR REGRESSION TESTING STEPS GO HERE'''.stripMargin())
-  }
-}
+// regressionTestJob.with{
+//   description("This job runs regression tests on the deployed skeleton application")
+//   parameters{
+//     stringParam("B",'',"Parent build number")
+//     stringParam("PARENT_BUILD","Skeleton_Application_Build","Parent build name")
+//     stringParam("ENVIRONMENT_NAME","CI","Name of the environment.")
+//   }
+//   logRotator {
+//     daysToKeep(logRotatorDaysToKeep)
+//     numToKeep(logRotatorBuildNumToKeep)
+//     artifactDaysToKeep(logRotatorArtifactsNumDaysToKeep)
+//     artifactNumToKeep(logRotatorArtifactsNumToKeep)
+//   }
+//   scm scmProvider.get(projectFolderName, regressionTestGitRepo, "*/master", "adop-jenkins-master", null)
+//   wrappers {
+//     preBuildCleanup()
+//     injectPasswords()
+//     maskPasswords()
+//     sshAgent("adop-jenkins-master")
+//   }
+//   environmentVariables {
+//       env('WORKSPACE_NAME',workspaceFolderName)
+//       env('PROJECT_NAME',projectFolderName)
+//   }
+//   label("docker")
+//   steps {
+//     shell('''## YOUR REGRESSION TESTING STEPS GO HERE'''.stripMargin())
+//   }
+// }
