3f3c33c345bf00e0be9c0101f95ac063883bdc5e
teresa.lachina <teresa.lachina@accenture.com>
2019-03-15 11:35:20 -0400

    jar -t

--- jenkins/jobs/dsl/skeleton_pipeline_jobs.groovy
+++ jenkins/jobs/dsl/skeleton_pipeline_jobs.groovy
@@ -86,7 +86,7 @@ buildAppJob.with{
       goals('verify')
       goals('install')
     }
-    shell('''my-app/target/my-app-1.0-SNAPSHOT.jar''')
+    shell('''jar -t my-app/target/my-app-1.0-SNAPSHOT.jar''')
   }
   publishers{
     downstream(projectFolderName + "/Testing_Job")
